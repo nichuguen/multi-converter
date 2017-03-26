938ec9c662823af09945cffbaee46f79bea33655
Joel Gugger <joel@thimoo.ch>
2017-03-26 23:35:54 +0200

    improving UI

--- application/converter/static/scripts/app.js
+++ application/converter/static/scripts/app.js
@@ -12,60 +12,141 @@
   $('#base-code').on('focus', function() { inFocus = true; });
   $('#base-code').on('focusout', function() { inFocus = false; });
 
-  // Auto-select
-  function bindTextareaFocus() {
-    $('#results-content textarea').each(function() {
-      $(this).on('focus', function(e) {
-        this.select();
-      });
-    });
-  }
-
-  // Create shortcut
-  $(window).keydown(function(e) {
-    if (e.which == 86 && !inFocus) { // V
-      $('#convert-submit').click();
-    } else if (e.which == 67 && !inFocus) { // C
-      $('#reset-btn').click();
+  // Droppable area
+  $('#droppable-result').droppable({
+    activeClass: 'droppable-result-highlight',
+    accept: '.result-panel',
+    tolerance: 'touch',
+    drop: function(e, ui) {
+      var result = ui.draggable;
+      $(result)
+        .draggable('destroy')
+        .css({
+          left: 0,
+          top: 0,
+          position: 'relative'
+        })
+        .toggleClass('col-sm-6')
+        .addClass('full-width')
+        .detach();
+      $(result).find('.panel')
+        .removeClass('panel-default')
+        .addClass('panel-info')
+        .find('.panel-title')
+        .loadTemplate("/static/scripts/templates/close.html", {}, {
+          overwriteCache: true,
+          append: true,
+          success: function() {
+            $(this).on('click', function() {
+              $(this).parents('.result-panel').remove();
+              reloadPipeline();
+            });
+          }
+        });
+      $(result).find('textarea')
+        .attr('disabled','disabled');
+      $('#piping-result')
+        .append(result);
+      reloadPipeline();
     }
   });
 
-  // Hide results header
+  // Create shortcut
+  var cmd = false;
+  $(window).keydown(function(e) {
+    // cmd = 224 & ctrl = 17
+    if (e.which == 224 || e.which == 17) {
+      cmd = true;
+    } else if (e.which == 86 && !inFocus && !cmd) { // V
+      $('#convert-submit').click();
+    } else if (e.which == 67 && !inFocus && !cmd) { // C
+      $('#reset-btn').click();
+    }
+  });
+  $(window).keyup(function(e) {
+    if (e.which == 224 || e.which == 17) {
+      cmd = false;
+    }
+  });
+
+  // Hide stuff
   $('#results-header').hide();
+  $('#droppable-result').hide();
   // Get input valid formats
   $.getJSON(
     '/static/json/types.json',
     {},
     function(formats, textStatus, jqXHR) {
-      $('#formats-controls').empty();
-      $.each(formats, function(index, format) {
-        var label = $('<label class="radio-inline"></label>');
-        var input = $('<input type="radio" required="required" name="input-format" id="input-' + format.id + '" value="' + format.id + '">');
-        var html = label.append(input).append(format.name);
-        $('#formats-controls').append(html);
-      });
+      $('#formats-controls')
+        .loadTemplate("/static/scripts/templates/label.html", formats);
     });
 
   $('#convert-form').on('submit', function(e) {
     e.preventDefault();
     $('#convert-submit').prop('disabled', true);
+    $('input[name=input-format]').prop('disabled', true);
+    $('#base-code').attr('disabled','disabled');
+    reloadPipeline();
+  });
+
+  $('#reset-btn').on('click', function() {
+    $('#base-code').focus();
+    $('#alert-content').empty();
+    $('#piping-result').empty();
+    $('#results-content').empty();
+    $('#results-header').hide();
+    $('#droppable-result').hide();
+    $('input[name=input-format]').prop('disabled', false);
+    $('#base-code').removeAttr('disabled');
+    setTimeout(function () { $('#convert-form')[0].reset(); }, 1);
+  });
+
+  // Reload the pipeline
+  function reloadPipeline() {
+    // Construct the pipeline
+    var pipeline = [];
+    $('#piping-result textarea').each(function(index, textarea) {
+      var ecodeId = $(textarea)
+        .parents('.result-panel')
+        .attr('data-id');
+      var step = {
+        step: index,
+        ecodeId: parseInt(ecodeId),
+      };
+      pipeline.push(step);
+    });
+    console.log(pipeline);
+
+    // TODO
+    // - get the original format
+    // - get the input value
+    // - code with input and format
+    // - update res array
+    // - if pipeline > 0
+    //   - for each step
+    //     - code with the previous output and current format
+    //     - update res array
+    // - update result UI with res array
+
     $.ajax(
         '/static/json/results.json?' + $('#convert-form').serialize(),
         { dataType: 'json' }
       ).done(function(results) {
         $('#alert-content')
-          .empty()
-          .prepend($('<div class="alert alert-success" role="alert"><strong>Converted!</strong> input converted correctly!</div>'));
-
+          .loadTemplate("/static/scripts/templates/message.html", {
+            classes: 'alert alert-success',
+            title: 'Converted!',
+            message: 'input converted correctly!'
+          });
         $('#results-content')
           .empty()
           .loadTemplate("/static/scripts/templates/results.html", results, {
             success: function() {
-              bindTextareaFocus();
+              bindEventOnResult();
             }
           });
-
         $('#results-header').show();
+        $('#droppable-result').show();
         $('html').animate({
           scrollTop: $("#results-header").offset().top
         }, 500, 'swing', function() {
@@ -74,19 +155,41 @@
 
       }).error(function(jqXHR, textStatus, errorThrown) {
         $('#alert-content')
-          .empty()
-          .prepend($('<div class="alert alert-danger" role="alert"><strong>Error!</strong> ' + errorThrown + '</div>'));
+          .loadTemplate("/static/scripts/templates/message.html", {
+            classes: 'alert alert-danger',
+            title: 'Error!',
+            message: errorThrown
+          });
       }).always(function() {
         $('#convert-submit').prop('disabled', false);
       });
-  });
+  }
 
-  $('#reset-btn').on('click', function() {
-    $('#base-code').focus();
-    $('#alert-content').empty();
-    setTimeout(function () { $('#convert-form')[0].reset(); }, 1);
-    $('#results-content').empty();
-    $('#results-header').hide();
-  });
+  // Auto-select
+  function bindEventOnResult() {
+    $('#results-content textarea').each(function() {
+      $(this).on('focus', function(e) { this.select(); });
+      $(this).on('onselectstart', function(e) { return false; });
+    });
+    $('#results-content .result-panel').each(function() {
+      $(this).draggable({
+        zIndex: 100,
+        revert: 'invalid',
+        revertDuration: 0,
+        snap: true,
+        snapMode: 'inner',
+        start: function(event, ui) {
+          $(this).find('.panel')
+            .removeClass('panel-default')
+            .addClass('panel-primary');
+        },
+        stop: function(event, ui) {
+          $(this).find('.panel')
+            .removeClass('panel-primary')
+            .addClass('panel-default');
+        }
+      });
+    });
+  }
 
 })(jQuery);
